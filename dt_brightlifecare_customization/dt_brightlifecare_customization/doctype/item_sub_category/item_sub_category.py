11562a54160efefde025c6f7c6ac03c7f757ef1c
karshin-uppal <dev4@dipanetech.com>
2025-05-06 11:17:05 +0530

    V23, item grp and item sub category working

---
+++ dt_brightlifecare_customization/dt_brightlifecare_customization/doctype/item_sub_category/item_sub_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Digitalis Technologies Pvt Ltd and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class ItemSubCategory(NestedSet):
+	pass
