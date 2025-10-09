8e3d74640de9d58978d7dbafc9fefaaece73781c
karshin-uppal <dev4@dipanetech.com>
2025-10-09 12:52:13 +0530

    v67, BPR

--- dt_brightlifecare_customization/dt_brightlifecare_customization/doctype/check_point_template/check_point_template.py
+++ dt_brightlifecare_customization/dt_brightlifecare_customization/doctype/check_point_template/check_point_template.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2025, Digitalis Technologies Pvt Ltd and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class CheckPointTemplate(Document):
-	pass
+    def validate(self):
+        # If this record is marked as default
+        if self.is_default:
+            # Uncheck 'is_default' in all other Check Point Templates
+            frappe.db.sql("""
+                UPDATE `tabCheck Point Template`
+                SET is_default = 0
+                WHERE name != %s
+            """, (self.name,))
