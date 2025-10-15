94bd7449a2a7f7c9934ffbc45f5061618ad5b5b3
karshin-uppal <dev4@dipanetech.com>
2025-10-15 13:14:48 +0530

    v78, is default if 1 document checked then others should be unchecekd BPR fetching tables

--- dt_brightlifecare_customization/dt_brightlifecare_customization/doctype/line_clearance_for_secondary_packing_area_template/line_clearance_for_secondary_packing_area_template.py
+++ dt_brightlifecare_customization/dt_brightlifecare_customization/doctype/line_clearance_for_secondary_packing_area_template/line_clearance_for_secondary_packing_area_template.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2025, Digitalis Technologies Pvt Ltd and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class LineClearanceforSecondaryPackingAreaTemplate(Document):
-	pass
+	def validate(self):
+		if self.is_default:
+			frappe.db.sql("""
+			UPDATE `tabLine Clearance for Secondary Packing Area Template`
+			SET is_default = 0
+			WHERE name != %s
+			""", (self.name,))
