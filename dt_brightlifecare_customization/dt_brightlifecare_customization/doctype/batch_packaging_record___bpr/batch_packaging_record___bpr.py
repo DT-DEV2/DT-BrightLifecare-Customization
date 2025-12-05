57d383cd6c69a05eb11ea464c22f13f8ab7e7e42
karshin-uppal <dev4@dipanetech.com>
2025-12-05 17:36:34 +0530

    v102, Machinery Setup Dispensing Request tab added in BPR and items fetched from BOM - revision number

--- dt_brightlifecare_customization/dt_brightlifecare_customization/doctype/batch_packaging_record___bpr/batch_packaging_record___bpr.py
+++ dt_brightlifecare_customization/dt_brightlifecare_customization/doctype/batch_packaging_record___bpr/batch_packaging_record___bpr.py
@@ -1,9 +1,27 @@
-# Copyright (c) 2025, Digitalis Technologies Pvt Ltd and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class BatchPackagingRecordBPR(Document):
-	pass
+    
+    def before_save(self):
+        bom_no = self.revision_no     # BOM name from your field
+
+        if not bom_no:
+            return
+
+        # fetch BOM items in the same order (idx order)
+        bom_items = frappe.get_all(
+            "BOM Item",
+            filters={"parent": bom_no},
+            fields=["item_code", "item_name", "idx"],
+            order_by="idx asc"
+        )
+
+        # clear child table first
+        self.machinery_setup_dispensing_request_detail = []
+
+        # append in the same order
+        for item in bom_items:
+            row = self.append("machinery_setup_dispensing_request_detail", {})
+            row.item_code = item.item_code
+            row.item_name = item.item_name
